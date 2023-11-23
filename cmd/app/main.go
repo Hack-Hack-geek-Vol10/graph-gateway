c67241cf4068bcc6f6fa72a06c22c38b2472144b
murasame <oogiriminister@gmail.com>
2023-11-24 00:41:46 +0900

    fix:filepath

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"github.com/Hack-Hack-geek-Vol10/graph-gateway/cmd/config"
+	"github.com/Hack-Hack-geek-Vol10/graph-gateway/pkg/google"
 	"github.com/Hack-Hack-geek-Vol10/graph-gateway/src/server"
-	"github.com/Hack-Hack-geek-Vol10/graph-gateway/utils/google"
 )
 
 func init() {
