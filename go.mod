ec6427337e075720a449f748a125cf5f271bfc17
sikigasa <km3t1114@yahoo.co.jp>
2023-12-09 01:52:55 +0900

    go mod tidy

--- go.mod
+++ go.mod
@@ -15,22 +15,15 @@ require (
 
 require (
 	github.com/agnivade/levenshtein v1.1.1 // indirect
-	github.com/cpuguy83/go-md2man/v2 v2.0.3 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/uuid v1.4.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.3 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/sosodev/duration v1.2.0 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
-	github.com/urfave/cli/v2 v2.26.0 // indirect
-	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
-	golang.org/x/mod v0.14.0 // indirect
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	golang.org/x/tools v0.16.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- src/gateways/save-client.go
+++ src/gateways/save-client.go
@@ -1,27 +1,25 @@
 package gateways
 
-import (
-	"context"
+// import (
+// 	"context"
 
-	saveService "github.com/schema-creator/graph-gateway/pkg/grpc/save-service/v1"
-)
+// 	saveService "github.com/schema-creator/graph-gateway/pkg/grpc/save-service/v1"
+// )
 
-type saveClient struct {
-	client saveService.SaveClient
-}
+// type saveClient struct {
+// 	client saveService.SaveClient
+// }
 
-type SaveClient interface {
+// type SaveClient interface {
+// 	CreateSave(ctx context.Context, arg *saveService.CreateSaveParams) (*saveService.SaveDetail, error)
+// }
 
-}
+// func NewSaveClient(client saveService.SaveClient) SaveClient {
+// 	return &saveClient{
+// 		client: client,
+// 	}
+// }
 
-func NewSaveClient(client saveService.SaveClient) SaveClient {
-	return &saveClient{
-		client: client,
-	}
-}
-
-func (s *saveClient) CreateSave(ctx context.Context, arg *saveService.CreateSaveParams) (*saveService.SaveDetail, error) {
-	return s.client.CreateSave(ctx, arg)
-}
-
-func (s *saveClient) 
+// func (s *saveClient) CreateSave(ctx context.Context, arg *saveService.CreateSaveParams) (*saveService.SaveDetail, error) {
+// 	return s.client.CreateSave(ctx, arg)
+// }
